cd04cd5b152df4ef85569798419e0b8a21309e76
Marcus Carvalho <marcusca@microsoft.com>
2020-02-23 22:35:38 +0100

    Moved key controller out of the SCIM library

---
+++ Microsoft.SCIM.WebHostSample/Controllers/TokenController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.SCIM.WebHostSample.Controllers
+{
+    // Controller for generating a bearer token for authorization during testing.
+    // This is not meant to replace proper Oauth for authentication purposes.
+    [Route(ServiceConstants.RouteToken)]
+    [ApiController]
+    public class KeyController : ControllerBase
+    {
+        private const int TokenLifetimeInMins = 120;
+
+        private static string GenerateJSONWebToken()
+        {
+            SymmetricSecurityKey securityKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ServiceConstants.TokenIssuer));
+            SigningCredentials credentials =
+                new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            DateTime startTime = DateTime.UtcNow;
+            DateTime expiryTime = startTime.AddMinutes(KeyController.TokenLifetimeInMins);
+
+            JwtSecurityToken token =
+                new JwtSecurityToken(
+                    ServiceConstants.TokenIssuer,
+                    ServiceConstants.TokenAudience,
+                    null,
+                    notBefore: startTime,
+                    expires: expiryTime,
+                    signingCredentials: credentials);
+
+            string result = new JwtSecurityTokenHandler().WriteToken(token);
+            return result;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string tokenString = KeyController.GenerateJSONWebToken();
+            return this.Ok(new { token = tokenString });
+        }
+
+    }
+}
