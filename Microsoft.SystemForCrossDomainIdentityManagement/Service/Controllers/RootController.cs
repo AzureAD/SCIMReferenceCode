0966397d5a505eb19756bb167c8ca19908968ce9
ArvindHarinder1 <36525136+ArvindHarinder1@users.noreply.github.com>
2020-02-25 17:59:44 -0800

    Update RootController.cs

--- Microsoft.SystemForCrossDomainIdentityManagement/Service/Controllers/RootController.cs
+++ Microsoft.SystemForCrossDomainIdentityManagement/Service/Controllers/RootController.cs
@@ -1,6 +1,4 @@
-﻿//------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.// Licensed under the MIT license.
 
 namespace Microsoft.SCIM
 {
